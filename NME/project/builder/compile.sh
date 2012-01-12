95acc9a58288a5e4f62dac1962d04ca07e2ddb12
Matthijs Kamstra <matthijs@noprotocol.nl>
2012-01-12 23:40:44 +0100

    update nme compile files (ant/shell/hxml)

--- NME/project/builder/compile.sh
+++ NME/project/builder/compile.sh
@@ -3,7 +3,11 @@
 clear
 echo '----------------------------------------------------------'
 echo 'Automation script for NME project "${projectName}"'
-echo '    - compile the config.nmml for different targets'
+echo 'Make sure that you have all the sdk-s installed'
+echo 'Read more about this:' 
+echo '     http://www.haxenme.org/developers/get-started/'
+echo ''
+echo 'Start compiling different targets'
 echo '----------------------------------------------------------'
 
 BASEDIR=$(dirname $0)
@@ -11,11 +15,28 @@ BASEDIR=$(dirname $0)
 
 # set dir to the 'root' dir 
 cd $BASEDIR/../
+echo '------ compile flash'
 nme test "config.nmml" flash
-nme test "config.nmml" ios -simulator
-nme test "config.nmml" webos
-nme test "config.nmml" android
-nme test "config.nmml" windows
-nme test "config.nmml" mac
-nme test "config.nmml" linux
+
+echo '------ compile html5'
 nme test "config.nmml" html5
+
+echo '------ compile window'
+nme test "config.nmml" windows
+
+echo '------ compile mac'
+nme test "config.nmml" mac
+
+echo '------ compile linux'
+nme test "config.nmml" linux
+
+echo '------ compile iOS (need XCode)'
+nme test "config.nmml" ios -simulator
+
+echo '------ compile webos (need webOS SDK)'
+nme test "config.nmml" webos
+
+echo '------ compile android (need Android SDK, Android NDK, Java JDK and Apache Ant)'
+nme test "config.nmml" android
+
+echo '------ end shell script'
